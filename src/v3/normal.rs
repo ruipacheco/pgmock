ad8c17d9d121e00f54eee5c7f5a68d280a617abb
Rui Pacheco <rui.pacheco@gmail.com>
2021-11-16 16:23:39 +0000

    Really delete files

--- src/v3/normal.rs
+++
@@ -1,7 +0,0 @@
-//! Messages sent by the server during the normal phase, after successful authentication.
-
-use super::super::{Messages, Server};
-
-pub(crate) enum Normal {}
-impl Messages for Normal {}
-impl Server<Normal> {}
